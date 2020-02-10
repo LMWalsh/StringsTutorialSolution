4e341afa8098c40bc618afc3c303611e20468dc6
max-student <max-student@maxtrain.com>
2020-02-10 17:53:52 -0500

    Initial commit

---
+++ StringsTutorialProject/StringsTutorial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringsTutorialProject {
+    class StringsTutorial {
+
+        public string Firstname = "Ben";
+        public string Lastname = "Walsh";
+
+        public StringsTutorial() {
+            Firstname = "Ben";
+            Lastname = "Walsh";
+            }
+
+        public StringsTutorial(string firstname, string lastname) {
+            Firstname = firstname;
+            Lastname = lastname; 
+            }
+
+        public string Fullname() {
+
+            //var Firstname = "Laura";
+            //var Lastname = "Walsh";
+
+            // var fullname = Firstname + " " + Lastname;
+            var fullname = $" {Lastname}, {Firstname}";
+
+            return fullname;
+            }
+        public string Fullname2() { 
+      //var Firstname = "Laura";
+      //  var Lastname = "Walsh";
+        var fullname = $" {Firstname} {Lastname}";
+        return fullname;
+        }
+    }
+    }
